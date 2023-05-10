1e20982b1f026e8ec03528c0b36431ddcf3f6d2e
abdhafizzzz <abdhafiz47@gmail.com>
2023-05-10 15:01:00 +0800

    first and completed commit

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Database\Schema\IndexDefinition;
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    public function index()
+    {
+        return 'This is index() method in a PostController';
+    }
+}

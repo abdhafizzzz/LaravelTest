48801c465606b047b7202a171eadc716da0f07e6
abdhafizzzz <abdhafiz47@gmail.com>
2023-05-11 10:53:34 +0800

    committed

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Database\Schema\IndexDefinition;
+//use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    public function index()
+   /* public function index()
     {
         return 'This is index() method in a PostController';
-    }
+    } */
 }
 
 
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Schema\IndexDefinition;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Contracts\Container\BindingResolutionException;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,14 +14,75 @@ use Illuminate\Contracts\Container\BindingResolutionException;
 |
 */
 
-Route::get('/', function () {
-
+/*Route::get('/', function () {
     return view('welcome');
-});
+}); */
 
-// Auth::routes();
+//Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/blog', [PostsController::class, 'index']);
-~
+//necessary(?) line for using resource method controller
+//Route::resource('blog', App\Http\Controllers\PRController::class);
+
+// //GET request
+// Route::get('/blog', [App\Http\Controllers\PostsController::class, 'index'])->name('blog'); //testing yay, 1st method
+// Route::get('/blog/1', [App\Http\Controllers\PostsController::class, 'show'])->name('blog'); //accessing one specific pos where your id is 1, wont be visible in index method but in show method
+
+// //POST
+// Route::get('/blog/create', [App\Http\Controllers\PostsController::class, 'create'])->name('blog');//will visible in create method in controller
+// Route::post('/blog', [App\Http\Controllers\PostsController::class, 'store'])->name('blog');//when user submitted the form, inside store method inside database
+
+// //PUT OR PATCH
+// Route::get('/blog/edit/1', [App\Http\Controllers\PostsController::class, 'edit'])->name('blog');//visible in specific id of 1 in edit method (controller)
+// Route::patch('/blog/1', [App\Http\Controllers\PostsController::class, 'update'])->name('blog');
+
+// //DELETE
+// Route::delete('/blog/1', [App\Http\Controllers\PostsController::class, 'destroy'])->name('blog');//deleting specific id of 1
+
+//all above are handled in resource method of controller
+//Route::resource('blog', App\Http\Controllers\PRController::class);
+
+//Multiple HTTP verbs
+// Route::match(['GET', 'POST'], '/blog',[App\Http\Controllers\PRController::class, 'index']);
+// Route::any('/blog',[App\Http\Controllers\PRController::class, 'index']);
+
+//Return view only
+// Route::view('/blog', 'blog.index', ['name' => 'Testing']);
+
+//this if a route for invoke method
+
+// Route::get('/', App\Http\Controllers\HController::class);
+
+/*
+    GET - Request a resource
+    POST - Create a new resource
+    PUT - Update a resource
+    PATCH - Modify a resource (modify value that is already changed)
+    DELETE - Delete a resource
+    OPTIONS - Ask the server which verbs are allowed
+*/
+
+//Variable Route
+
+//GET request
+Route::get('/blog', [App\Http\Controllers\PRController::class, 'index'])->name('blog'); //
+Route::get('/blog/{id}', [App\Http\Controllers\PRController::class, 'show'])->name('blog'); //curly braces for variables
+
+//POST
+Route::get('/blog/create', [App\Http\Controllers\PRController::class, 'create'])->name('blog');//will visible in create method in controller
+Route::post('/blog', [App\Http\Controllers\PRController::class, 'store'])->name('blog');//when user submitted the form, inside store method inside database
+
+//PUT OR PATCH
+Route::get('/blog/edit/{id}', [App\Http\Controllers\PRController::class, 'edit'])->name('blog');//visible in specific id of 1 in edit method (controller)
+Route::patch('/blog/{id}', [App\Http\Controllers\PRController::class, 'update'])->name('blog');
+
+//DELETE
+Route::delete('/blog/{id}', [App\Http\Controllers\PRController::class, 'destroy'])->name('blog');//deleting specific id of 1
+
+//all above are handled in resource method of controller
+Route::resource('blog', App\Http\Controllers\PRController::class);
+
+//Multiple HTTP verbs
+Route::match(['GET', 'POST'], '/blog',[App\Http\Controllers\PRController::class, 'index']);
+Route::any('/blog',[App\Http\Controllers\PRController::class, 'index']);
